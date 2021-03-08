c8305fe68860350c677fd57da0ac7dd0e54c07bf
Daniele Serfilippi <sermicromegas@users.noreply.github.com>
2021-03-08 15:48:45 +0100

    Authentication management - Added fake backend server - Added api-client - Added useAsync for managing async operations - Splitted app into Authenticated/Unauthenticated components

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,17 +1,57 @@
 import ReactDOM from 'react-dom'
-import {screen} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import {screen, waitForElementToBeRemoved, within} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import faker from 'faker'
+import {server} from 'test/server'
 
-test('renders the app', () => {
+// enable API mocking in test runs using the same request handlers
+// as for the client-side mocking.
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
+
+// this is a pretty comprehensive test and CI is pretty slow...
+jest.setTimeout(25000)
+
+function buildUser(overrides) {
+  return {
+    id: faker.random.uuid(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    ...overrides,
+  }
+}
+
+const waitForLoadingToFinish = () =>
+  waitForElementToBeRemoved(
+    () => [
+      ...screen.queryAllByLabelText(/loading/i),
+      ...screen.queryAllByText(/loading/i),
+    ],
+    {timeout: 4000},
+  )
+
+test('can login and use the book search', async () => {
   const root = document.createElement('div')
   root.id = 'root'
   document.body.append(root)
 
   require('..')
 
-  screen.getByTitle('Bookshelf')
-  screen.getByRole('heading', {name: /Bookshelf/i})
-  screen.getByRole('button', {name: /Login/i})
-  screen.getByRole('button', {name: /Register/i})
+  const user = buildUser()
+
+  userEvent.click(await screen.findByRole('button', {name: /register/i}))
+
+  const modal = within(screen.getByRole('dialog'))
+  userEvent.type(modal.getByLabelText(/username/i), user.username)
+  userEvent.type(modal.getByLabelText(/password/i), user.password)
+
+  userEvent.click(modal.getByRole('button', {name: /register/i}))
+
+  await waitForLoadingToFinish()
+
+  userEvent.click(screen.getByRole('button', {name: /logout/i}))
 
   // cleanup
   ReactDOM.unmountComponentAtNode(root)
--- src/components/lib.js
+++ src/components/lib.js
@@ -1,37 +1,17 @@
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+
+import {Link as RouterLink} from 'react-router-dom'
 import styled from '@emotion/styled/macro'
 import {keyframes} from '@emotion/core'
+import * as colors from 'styles/colors'
+import * as mq from 'styles/media-queries'
 import {Dialog as ReachDialog} from '@reach/dialog'
 import {FaSpinner} from 'react-icons/fa'
-import * as mq from 'styles/media-queries'
-import * as colors from 'styles/colors'
 
-const buttonVariants = {
-  primary: {
-    background: colors.indigo,
-    color: colors.base,
-  },
-  secondary: {background: colors.gray, color: colors.text},
-}
-const Button = styled.button(
-  {
-    padding: '10px 15px',
-    border: '0',
-    lineHeight: '1',
-    borderRadius: '3px',
-  },
-  ({variant}) => buttonVariants[variant],
-)
-
-const Input = styled.input({
-  borderRadius: '3px',
-  border: `1px solid ${colors.gray10}`,
-  background: colors.gray,
-  padding: '8px 12px',
-})
-
-const FormGroup = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
+const spin = keyframes({
+  '0%': {transform: 'rotate(0deg)'},
+  '100%': {transform: 'rotate(360deg)'},
 })
 
 const CircleButton = styled.button({
@@ -49,6 +29,50 @@ const CircleButton = styled.button({
   cursor: 'pointer',
 })
 
+const BookListUL = styled.ul({
+  listStyle: 'none',
+  padding: '0',
+  display: 'grid',
+  gridTemplateRows: 'repeat(auto-fill, minmax(100px, 1fr))',
+  gridGap: '1em',
+})
+
+const Spinner = styled(FaSpinner)({
+  animation: `${spin} 1s linear infinite`,
+})
+Spinner.defaultProps = {
+  'aria-label': 'loading',
+}
+
+const buttonVariants = {
+  primary: {
+    background: colors.indigo,
+    color: colors.base,
+  },
+  secondary: {
+    background: colors.gray,
+    color: colors.text,
+  },
+}
+const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+  },
+  ({variant = 'primary'}) => buttonVariants[variant],
+)
+
+const inputStyles = {
+  border: '1px solid #f1f1f4',
+  background: '#f1f2f7',
+  padding: '8px 12px',
+}
+
+const Input = styled.input({borderRadius: '3px'}, inputStyles)
+const Textarea = styled.textarea(inputStyles)
+
 const Dialog = styled(ReachDialog)({
   maxWidth: '450px',
   borderRadius: '3px',
@@ -61,15 +85,91 @@ const Dialog = styled(ReachDialog)({
   },
 })
 
-const spin = keyframes({
-  '0%': {transform: 'rotate(0deg)'},
-  '100%': {transform: 'rotate(360deg)'},
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
 })
-const Spinner = styled(FaSpinner)({
-  animation: `${spin} 1s linear infinite`,
-})
-Spinner.defaultProps = {
-  'aria-label': 'loading',
+
+function FullPageSpinner() {
+  return (
+    <div
+      css={{
+        fontSize: '4em',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Spinner />
+    </div>
+  )
 }
 
-export {Button, CircleButton, Dialog, FormGroup, Input, Spinner}
+const Link = styled(RouterLink)({
+  color: colors.indigo,
+  ':hover': {
+    color: colors.indigoDarken10,
+    textDecoration: 'underline',
+  },
+})
+
+const errorMessageVariants = {
+  stacked: {display: 'block'},
+  inline: {display: 'inline-block'},
+}
+
+function ErrorMessage({error, variant = 'stacked', ...props}) {
+  return (
+    <div
+      role="alert"
+      css={[{color: colors.danger}, errorMessageVariants[variant]]}
+      {...props}
+    >
+      <span>There was an error: </span>
+      <pre
+        css={[
+          {whiteSpace: 'break-spaces', margin: '0', marginBottom: -5},
+          errorMessageVariants[variant],
+        ]}
+      >
+        {error.message}
+      </pre>
+    </div>
+  )
+}
+
+function FullPageErrorFallback({error}) {
+  return (
+    <div
+      role="alert"
+      css={{
+        color: colors.danger,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
+  )
+}
+
+export {
+  FullPageErrorFallback,
+  ErrorMessage,
+  CircleButton,
+  BookListUL,
+  Spinner,
+  Button,
+  Input,
+  Textarea,
+  Dialog,
+  FormGroup,
+  FullPageSpinner,
+  Link,
+}
